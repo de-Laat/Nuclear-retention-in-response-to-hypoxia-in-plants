79fd9bc83a1af39b25fcd34d7b1fce2eeb5c7699
GitteLans <127954517+GitteLans@users.noreply.github.com>
2023-06-14 11:04:44 +0200

    Update Trim.sh

--- Scripts/Trim.sh
+++ Scripts/Trim.sh
@@ -1,1 +1,6 @@
-
+# loop through all files in directory ending with .fastq.gz
+for file in t*.fasta; do
+  # run fastx_trimmer -z command on each file
+#   fastq_to_fasta -i "$file" -o "${file}.fasta"
+  fastx_trimmer -i "$file" -o "trim${file}"
+done
